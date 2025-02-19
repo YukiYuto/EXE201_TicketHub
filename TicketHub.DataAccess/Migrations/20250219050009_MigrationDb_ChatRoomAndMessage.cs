0562fb7a22fa61055ec9630980e229f355052621
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-02-19 12:07:00 +0700

    Add Migration ChatRoom and Message

---
+++ TicketHub.DataAccess/Migrations/20250219050009_MigrationDb_ChatRoomAndMessage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketHub.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationDb_ChatRoomAndMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatRooms",
+                columns: table => new
+                {
+                    ChatRoomId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameRoom = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    SendMessageUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ReceiveMessageUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatRooms", x => x.ChatRoomId);
+                    table.ForeignKey(
+                        name: "FK_ChatRooms_AspNetUsers_ReceiveMessageUserId",
+                        column: x => x.ReceiveMessageUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ChatRooms_AspNetUsers_SendMessageUserId",
+                        column: x => x.SendMessageUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MessageContent = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    SendMessageUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReceiveMessageUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ChatRoomId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_Messages_ChatRooms_ChatRoomId",
+                        column: x => x.ChatRoomId,
+                        principalTable: "ChatRooms",
+                        principalColumn: "ChatRoomId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatRooms_ReceiveMessageUserId",
+                table: "ChatRooms",
+                column: "ReceiveMessageUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatRooms_SendMessageUserId",
+                table: "ChatRooms",
+                column: "SendMessageUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatRoomId",
+                table: "Messages",
+                column: "ChatRoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "ChatRooms");
+        }
+    }
+}
